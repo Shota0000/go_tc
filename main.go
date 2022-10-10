1ccdd89737d171c80aa797a5e99ba88f85a017d6
Shota0000 <supercord530@icloud.com>
2022-10-10 19:06:12 +0900

    commitさせろ

--- main.go
+++ main.go
@@ -19,81 +19,88 @@ func main() {
 		{
 			Name:  "delay",
 			Usage: "if use set -i or --ip ",
-			Flags: []cli.Flag{
-				cli.StringFlag{
-					Name:  "t, time",
-					Value: "",
-					Usage: "delay -t time -i ip",
+			Subcommands: []cli.Command{
+				{
+					Name:  "reset",
+					Usage: "delay reset",
+					Action: func(c *cli.Context) error {
+						cmd, _ := shellwords.Parse("tc qdisc del dev eth0 root")
+						out, err := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+						if err != nil {
+							fmt.Println(err)
+						}
+						fmt.Println(string(out))
+						return nil
+					},
 				},
-				cli.StringSliceFlag{
-					Name:  "i, ip",
-					Usage: "delay -t time -i ip -i ip ...",
+				{
+					Name:  "add",
+					Usage: "delay add -t time -p ip ...",
+					Flags: []cli.Flag{
+						cli.StringFlag{
+							Name:  "t, time",
+							Value: "",
+							Usage: "delay -t time -i ip",
+						},
+						cli.StringSliceFlag{
+							Name:  "i, ip",
+							Usage: "delay -t time -i ip -i ip ...",
+						},
+					},
+					Action: func(c *cli.Context) error {
+						cmd, _ := shellwords.Parse("tc qdisc add dev eth0 root handle 1: htb default 1")
+						out, err := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+						if err != nil {
+							fmt.Println(err)
+						}
+						fmt.Println(string(out))
+
+						cmd, _ = shellwords.Parse("tc class add dev eth0 parent 1: classid 1:1 htb rate 100Gbit")
+						out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+						if err != nil {
+							fmt.Println(err)
+						}
+						fmt.Println(string(out))
+
+						// add qdisc to class
+						for i := 1; i <= len(c.StringSlice("ip")); i++ {
+							cmd, _ = shellwords.Parse(fmt.Sprint("tc class add dev eth0 parent 1:1 classid 1:", i, "0 htb rate 10Gbit"))
+							out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+							if err != nil {
+								fmt.Println(err)
+							}
+							fmt.Println(string(out))
+							fmt.Println(fmt.Sprint("tc class add dev eth0 parent 1:1 classid 1:", i, "0 htb rate 10Gbit"))
+						}
+
+						// shell scriptのcreate classを実装
+						for i := 1; i <= len(c.StringSlice("ip")); i++ {
+							cmd, _ = shellwords.Parse(fmt.Sprint("tc qdisc add dev eth0 parent 1:", i, "0 handle 1", i, ": netem delay ", c.String("time")))
+							out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+							if err != nil {
+								fmt.Println(err)
+							}
+							fmt.Println(string(out))
+							fmt.Println(fmt.Sprint("tc qdisc add dev eth0 parent 1:", i, "0 handle 1", i, ": netem delay ", c.String("time")))
+
+						}
+
+						// shell scriptのadd filterを実装
+						for i := 1; i <= len(c.StringSlice("ip")); i++ {
+							cmd, _ = shellwords.Parse(fmt.Sprint("tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst ", c.StringSlice("ip")[i-1], " flowid 1:", i, "0"))
+							out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
+							if err != nil {
+								fmt.Println(err)
+							}
+							fmt.Println(string(out))
+							fmt.Println(fmt.Sprint("tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst ", c.StringSlice("ip")[i-1], " flowid 1:", i, "0"))
+						}
+
+						return nil
+					},
 				},
-			},
-			Action: func(c *cli.Context) error {
-				fmt.Println(c.StringSlice("ip"))
-				cmd, _ := shellwords.Parse("tc qdisc del dev eth0 root")
-				out, err := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println(string(out))
-
-				cmd, _ = shellwords.Parse("tc qdisc add dev eth0 root handle 1: htb default 1")
-				out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println(string(out))
-
-				cmd, _ = shellwords.Parse("tc class add dev eth0 parent 1: classid 1:1 htb rate 100Gbit")
-				out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
-				if err != nil {
-					fmt.Println(err)
-				}
-				fmt.Println(string(out))
-
-				// add qdisc to class
-				for i := 1; i <= len(c.StringSlice("ip")); i++ {
-					cmd, _ = shellwords.Parse(fmt.Sprint("tc class add dev eth0 parent 1:1 classid 1:", i, "0 htb rate 10Gbit"))
-					out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
-					if err != nil {
-						fmt.Println(err)
-					}
-					fmt.Println(string(out))
-					fmt.Println(fmt.Sprint("tc class add dev eth0 parent 1:1 classid 1:", i, "0 htb rate 10Gbit"))
-				}
-
-				// shell scriptのcreate classを実装
-				for i := 1; i <= len(c.StringSlice("ip")); i++ {
-					cmd, _ = shellwords.Parse(fmt.Sprint("tc qdisc add dev eth0 parent 1:", i, "0 handle 1", i, ": netem delay ", c.String("time")))
-					out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
-					if err != nil {
-						fmt.Println(err)
-					}
-					fmt.Println(string(out))
-					fmt.Println(fmt.Sprint("tc qdisc add dev eth0 parent 1:", i, "0 handle 1", i, ": netem delay ", c.String("time")))
-
-				}
-
-				// shell scriptのadd filterを実装
-				for i := 1; i <= len(c.StringSlice("ip")); i++ {
-					cmd, _ = shellwords.Parse(fmt.Sprint("tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst ", c.StringSlice("ip")[i-1], " flowid 1:", i, "0"))
-					out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
-					if err != nil {
-						fmt.Println(err)
-					}
-					fmt.Println(string(out))
-					fmt.Println(fmt.Sprint("tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst ", c.StringSlice("ip")[i-1], " flowid 1:", i, "0"))
-				}
-
-				return nil
-
-				// TO DO tcコマンド自体が機能してなさそう．なぜ?
-
 			},
 		},
 	}
-
 	app.Run(os.Args)
 }
