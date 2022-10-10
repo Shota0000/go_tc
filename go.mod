b25ce69f356918c21f48efa161aaffefd8c23a85
Shota0000 <supercord530@icloud.com>
2022-10-10 13:58:59 +0900

    サブコマンドなし

--- go.mod
+++ go.mod
@@ -2,7 +2,10 @@ module go_tc
 
 go 1.17
 
-require github.com/urfave/cli v1.22.10
+require (
+	github.com/mattn/go-shellwords v1.0.12
+	github.com/urfave/cli v1.22.10
+)
 
 require (
 	github.com/cpuguy83/go-md2man/v2 v2.0.0-20190314233015-f79a8a8ca69d // indirect
--- main.go
+++ main.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 
+	"github.com/mattn/go-shellwords"
 	"github.com/urfave/cli"
 )
 
@@ -26,59 +27,69 @@ func main() {
 				},
 				cli.StringSliceFlag{
 					Name:  "i, ip",
-					Usage: "delay -t time -i ip",
+					Usage: "delay -t time -i ip -i ip ...",
 				},
 			},
 			Action: func(c *cli.Context) error {
-				// fmt.Printf("Hello %s %s \n", c.StringSlice("ip"), c.String("time"))
-				out, err := exec.Command("tc", "qdisc del dev eth0 root").Output()
+				fmt.Println(c.StringSlice("ip"))
+				cmd, _ := shellwords.Parse("tc qdisc del dev eth0 root")
+				out, err := exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
 				if err != nil {
 					fmt.Println(err)
 				}
 				fmt.Println(string(out))
 
-				out, err = exec.Command("tc", "qdisc add dev eth0 root handle 1: htb default 10").Output()
+				cmd, _ = shellwords.Parse("tc qdisc add dev eth0 root handle 1: htb default 1")
+				out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
 				if err != nil {
 					fmt.Println(err)
 				}
 				fmt.Println(string(out))
 
-				out, err = exec.Command("tc", "class add dev eth0 parent 1: classid 1:1 htb rate 100Gbit").Output()
+				cmd, _ = shellwords.Parse("tc class add dev eth0 parent 1: classid 1:1 htb rate 100Gbit")
+				out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
 				if err != nil {
 					fmt.Println(err)
 				}
 				fmt.Println(string(out))
 
 				// add qdisc to class
-				for i := 0; i < len(c.StringSlice("ip")); i++ {
-					out, err = exec.Command("tc", fmt.Sprint("class add dev eth0 parent 1:1 classid 1:", i, "0 htb rate 10Gbit")).Output()
+				for i := 1; i <= len(c.StringSlice("ip")); i++ {
+					cmd, _ = shellwords.Parse(fmt.Sprint("tc class add dev eth0 parent 1:1 classid 1:", i, "0 htb rate 10Gbit"))
+					out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
 					if err != nil {
 						fmt.Println(err)
 					}
 					fmt.Println(string(out))
+					fmt.Println(fmt.Sprint("tc class add dev eth0 parent 1:1 classid 1:", i, "0 htb rate 10Gbit"))
 				}
 
 				// shell scriptのcreate classを実装
-				for i := 0; i < len(c.StringSlice("ip")); i++ {
-					out, err = exec.Command("tc", fmt.Sprint("qdisc add dev eth0 parent 1:", i, "0 handle 1", i, ": netem delay 100ms")).Output()
+				for i := 1; i <= len(c.StringSlice("ip")); i++ {
+					cmd, _ = shellwords.Parse(fmt.Sprint("tc qdisc add dev eth0 parent 1:", i, "0 handle 1", i, ": netem delay ", c.String("time")))
+					out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
 					if err != nil {
 						fmt.Println(err)
 					}
 					fmt.Println(string(out))
+					fmt.Println(fmt.Sprint("tc qdisc add dev eth0 parent 1:", i, "0 handle 1", i, ": netem delay ", c.String("time")))
+
 				}
 
 				// shell scriptのadd filterを実装
-				for i := 0; i < len(c.StringSlice("ip")); i++ {
-					out, err = exec.Command("tc", fmt.Sprint("filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst ", c.StringSlice("ip")[i], " flowid 1:", i, "0")).Output()
+				for i := 1; i <= len(c.StringSlice("ip")); i++ {
+					cmd, _ = shellwords.Parse(fmt.Sprint("tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst ", c.StringSlice("ip")[i-1], " flowid 1:", i, "0"))
+					out, err = exec.Command(cmd[0], cmd[1:]...).CombinedOutput()
 					if err != nil {
 						fmt.Println(err)
 					}
 					fmt.Println(string(out))
+					fmt.Println(fmt.Sprint("tc filter add dev eth0 protocol ip parent 1: prio 1 u32 match ip dst ", c.StringSlice("ip")[i-1], " flowid 1:", i, "0"))
 				}
 
 				return nil
 
-				// TO DO macでtcコマンド動かんからdocker立ち上げて動かす
+				// TO DO tcコマンド自体が機能してなさそう．なぜ?
 
 			},
 		},
